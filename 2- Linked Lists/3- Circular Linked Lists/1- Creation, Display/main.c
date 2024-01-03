21931e44d72fd5da22c6a2b2fdb18d3d1593eb8b
Tolga <tolgaboz350@gmail.com>
2024-01-03 07:32:27 +0300

    Refactor code for improved readability

--- 2- Linked Lists/3- Circular Linked Lists/1- Creation, Display/main.c
+++ 2- Linked Lists/3- Circular Linked Lists/1- Creation, Display/main.c
@@ -8,54 +8,60 @@ struct node {
 };
 
 // Function Prototypes
-struct node *createNewNode(struct node *root);
+struct node *createNewNode();
+struct node *insertBegin(struct node *root);
 void printList(struct node *root);
 
 int main(int argc, char *argv[]) {
 	// Initialize the root of the circular linked list
-    struct node* root = NULL;
-
-	// Insert elements at the end of the list	
-	root = createNewNode(root);
-	root = createNewNode(root);
-	root = createNewNode(root);
-	root = createNewNode(root);
+    struct node *root = NULL;
+    
+	// Insert elements at the begin of the list
+	root = insertBegin(root);
+	root = insertBegin(root);
+	root = insertBegin(root);
+	root = insertBegin(root);
 	
 	// Display the circular linked list
 	printList(root);
 	return 0;
 }
 
-// Function to create a new node at the end of the circular linked list
-struct node *createNewNode(struct node *root) {
-	struct node *newNode = (struct node *)malloc(sizeof(struct node));
-	newNode->next = newNode;
-	
-	printf("Enter a value: ");
-	scanf("%d", &(newNode->data));
-	
-	// If the list is empty, set the new node as the root
+// Function to create a new node and return it to insert function
+struct node *createNewNode() {
+    struct node *newNode = (struct node *)malloc(sizeof(struct node));
+    if(newNode == NULL) {
+	    perror("Memory allocation failed");
+	    exit(EXIT_FAILURE);
+	}
+    newNode->next = newNode;
+    return newNode;
+}
+
+// Function to create a new node at the begin of the circular linked list
+struct node *insertBegin(struct node *root) {
+	struct node *newNode = createNewNode();
+	printf("Enter data: ");
+    scanf("%d", &(newNode->data));
+    
 	if(root == NULL) {
+		// If the list is empty
 		root = newNode;
 	} else {
-		struct node *iter = root;
-		
-		// Find the last node of the list
-		do {
-			iter = iter->next;
-		} while(iter->next != root);
-		
-		iter->next = newNode;
-		newNode->next = root;
+		newNode->next = root->next;
+        root->next = newNode;
+        root = newNode;
 	}
+	printf("%d inserted at the beginning of the circular linked list.\n\n", root->data);
+	
 	// Return updated root
 	return root;
 }
 
 // Function to display the circular linked list
 void printList(struct node *root) {
-	// Check if the list is empty
 	if(root == NULL) {
+		// Check if the list is empty
 		printf("List is empty!\n");
 	} else {
 		struct node *iter = root;
